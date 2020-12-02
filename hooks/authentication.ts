80df759e7cef1c0ad775456aab6a85377d61280e
Nishino0719 <nishino0719@icloud.com>
2020-12-02 13:59:41 +0900

    :truck: fix(auth): change file lib -> hooks

---
+++ hooks/authentication.ts
@@ -0,0 +1,40 @@
+import firebase from "firebase/app";
+import { useEffect } from "react";
+import { atom, useRecoilState } from "recoil";
+import { User } from "../models/User";
+
+const userState = atom<User>({
+  key: "user",
+  default: null,
+});
+
+export function useAuthentication() {
+  const [user, setUser] = useRecoilState(userState);
+
+  useEffect(() => {
+    if (user !== null) {
+      return;
+    }
+
+    firebase
+      .auth()
+      .signInAnonymously()
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    firebase.auth().onAuthStateChanged(function (firebaseUser) {
+      if (firebaseUser) {
+        setUser({
+          uid: firebaseUser.uid,
+          isAnonymous: firebaseUser.isAnonymous,
+        });
+      } else {
+        //User is signed out
+        setUser(null);
+      }
+    });
+  }, []);
+
+  return { user };
+}
